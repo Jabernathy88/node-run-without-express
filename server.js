e60b794286a0762195988258d51a24dcc947494e
Jeremy Abernathy <jea@JEREMYs-MacBook-Pro.local>
2021-05-11 15:53:14 -0400

    GET root url.

--- server.js
+++ server.js
@@ -1,2 +1,13 @@
 const http = require('http')
 // more 
+const server = http.createServer((req, res) => {
+  console.log("GET ROOT!")
+  res.statusCode = 200
+  res.setHeader('Content-Type', 'text/html')
+  res.write('<h2>Yolo World.<h2>')
+  res.end()
+})
+
+const PORT = process.env.PORT || 2021
+
+server.listen(PORT, () => { console.log('Server listening on Port: ', PORT) })
